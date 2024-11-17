038a93e6d26a0b800b35ad2238b3042190e2ab18
Randeep Singh <balrandeep98@gmail.com>
2024-11-17 14:20:57 -0800

    cors enabled

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 import joblib
+from flask_cors import CORS
 
 #Loading the model
 with open("parking_model.pkl", "rb") as f:
     model = joblib.load(f);
 
 app = Flask(__name__)
+CORS(app)
 
 if __name__ == '__main__':
     app.run(port=5004)
